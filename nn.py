2c2b2bb990bc89b5786d6fe90acb5e7dc0959f92
ZhongyiYe <30846878+johnyeah@users.noreply.github.com>
2020-09-02 16:44:19 +0200

    Update and rename NN1.py to nn.py

---
+++ nn.py
@@ -0,0 +1,235 @@
+# load packages
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import colors
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as Data
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+from torch.nn import init
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import MinMaxScaler, Normalizer
+
+
+torch.manual_seed(999)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(999)
+
+
+# build net model
+class Net(nn.Module):
+    def __init__(self, n_input, n_hidden, n_output):
+        super(Net, self).__init__()
+        self.hidden1 = torch.nn.Linear(n_input, n_hidden)
+        self.hidden2 = torch.nn.Linear(n_hidden, n_hidden)
+        self.predict = torch.nn.Linear(n_hidden, n_output)
+
+    def forward(self, input):
+        out = self.hidden1(input)
+        out = F.relu(out)
+        out = self.hidden2(out)
+        out = torch.sigmoid(out)
+        out = self.predict(out)
+
+        return out
+
+
+
+
+
+
+'''
+data preprocessing
+'''
+FILE_PATH_TRAIN_static = '191217_test_record_iter_fix_static1.csv' # static obstacle csv file
+FILE_PATH_TRAIN_dynamic = 'test_record_iter_fix_static.csv'  # dynamic obstacle csv file
+
+
+# read csv file
+origin_data = pd.read_csv(FILE_PATH_TRAIN_static)
+
+# select the needed data by columns index
+features = ['grip_pos_x', 'grip_pos_y', 'grip_pos_z',
+            'grip_vel_x', 'grip_vel_y', 'grip_vel_z',
+            'obst_pos_x_1', 'obst_pos_y_1', 'obst_pos_z_1',
+            'goal_pos_x', 'goal_pos_y', 'goal_pos_z',
+            'action_x', 'action_y', 'action_z']
+
+label = 'is_safe_action'
+
+all_features = features + [label]
+
+origin_data[label] = origin_data[label].astype(int)
+origin_data = origin_data.loc[:, all_features]
+
+
+
+def balance_data():
+    # select safe_state & unsafe state out, then make their number be equal
+    n_safe_state = origin_data.loc[origin_data[label] == 1]
+    n_unsafe_state = origin_data.loc[origin_data[label] == 0]
+    n_safe_state = n_safe_state.sample(n=len(n_unsafe_state))  # make the number of safe and unsafe be equal
+
+    data = pd.concat([n_safe_state, n_unsafe_state], axis=0)  # concatenate safe samples and unsafe samples
+    data = data.sample(frac=1)  # randomization
+
+    return data
+
+
+
+
+
+def trainer(input_data):
+    num_epoch = 20
+
+    all_losses = []
+    all_acc = []
+
+    for epoch in range(num_epoch):
+         running_loss = 0.0
+         running_correct = 0
+
+
+         for batch_id, data in enumerate(input_data):
+             train_x, train_y = data
+
+             #zero the parameter gradients
+             optimizer.zero_grad()
+
+             #forward + backward + optimize
+             out = net(train_x)
+             loss = loss_func(out, train_y)
+             loss.backward()
+             optimizer.step()
+
+             # print statistics
+             running_loss += out.shape[0] * loss.item()
+             # running_loss += loss.item()
+
+             _, preds = torch.max(out, 1)
+             running_correct += torch.sum(preds == train_y).item()
+
+         epoch_loss = running_loss / len(train_loader.dataset)
+         all_losses.append(epoch_loss)
+
+         # accuracy in each epoch
+         epoch_acc = running_correct / len(train_loader.dataset)
+         all_acc.append(epoch_acc)
+
+         # nn has no mean and std deviation, because it only make decision one time.
+         # std deviation in each epoch
+         # epoch_std = np.std(running_correct)
+         # all_std.append(epoch_std)
+
+         print('Epoch', epoch, 'Loss', epoch_loss, 'Accuracy', epoch_acc)
+
+
+    # plot loss
+    plt.subplot(211)
+    plt.plot(np.arange(0, num_epoch), all_losses, label='loss')
+    plt.xlabel('epoch')
+    plt.ylabel('Loss')
+
+    plt.subplot(212)
+    plt.plot(np.arange(0, num_epoch), all_acc, label='accuracy')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.show()
+
+
+
+def test(input_data):
+    net1 = Net(n_input=14, n_hidden=20, n_output=2)
+    net1.load_state_dict(torch.load(PATH))
+    test_correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for batch_id, data in enumerate(input_data):
+            test_x, test_y = data
+            out = net1(test_x)
+            _, test_predict = torch.max(out.data, 1)
+            total += test_y.size(0)
+            test_correct += torch.sum(test_predict == test_y).item()
+
+    print('Acurracy:', 100 * test_correct / total)
+    np.save('/home/mae/Python_Code/CyberPortect_BNN/CyberPortect_BNN/y_nn_test_static', test_y)
+    np.save('/home/mae/Python_Code/CyberPortect_BNN/CyberPortect_BNN/y_nn_predict_static', test_predict)
+
+
+
+
+
+if __name__ == '__main__':
+    '''
+    prepare train data and test data
+    '''
+    # prepare features and label: x, y
+    preprocessed_data = balance_data()
+    preprocessed_data = preprocessed_data.values
+    x = preprocessed_data[:, 0:len(features) - 1]
+    y = preprocessed_data[:, len(all_features) - 1]
+
+    # splite data to train data and test data
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.01, random_state=42)
+
+    x_train, y_train = torch.FloatTensor(x_train), torch.LongTensor(y_train)
+    x_test, y_test = torch.FloatTensor(x_test), torch.LongTensor(y_test)
+
+    '''
+    x_train = torch.from_numpy(x_train).type(torch.FloatTensor)  # FloatTensor = 32-bit floating
+    y_train = torch.from_numpy(y_train).type(torch.LongTensor)  # LongTensor = 64-bit integer
+
+    x_test = torch.from_numpy(x_test).type(torch.FloatTensor)
+    y_test = torch.from_numpy(y_test).type(torch.LongTensor)
+    '''
+
+    torch_train_dataset = Data.TensorDataset(x_train, y_train)
+    torch_test_dataset = Data.TensorDataset(x_test, y_test)
+
+
+    train_loader = DataLoader(
+        dataset=torch_train_dataset,
+        batch_size=64,
+        shuffle=True,
+        num_workers=2,
+    )
+
+    test_loader = DataLoader(
+        dataset=torch_test_dataset,
+        batch_size=64,
+        shuffle=True,
+        num_workers=2,
+    )
+
+    #define network
+    net = Net(n_input=14, n_hidden=20, n_output=2)
+    print(net)
+
+    '''
+    Optimizer
+    '''
+    optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+    loss_func = nn.CrossEntropyLoss()
+
+    #train model
+    trainer(train_loader)
+
+    #save the trained model
+    PATH = 'net_params.pkl'
+    torch.save(net.state_dict(), PATH)
+
+    # test model
+    test(test_loader)
+
+
+
